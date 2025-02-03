46c4a3fea782b743a4ca419200c002d89a2cbca2
Romigo97 <roma.kolyada939@gmail.com>
2025-02-03 16:35:03 +0300

    Update models.py

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -37,13 +37,15 @@ class PokemonEntity(models.Model):
         Pokemon,
         on_delete=models.CASCADE,
         verbose_name='покемон',
-        related_name='entities')
+        related_name='entities'
+        )
     lat = models.FloatField(verbose_name='Широта')
     lon = models.FloatField(verbose_name='Долгота')
     appear_at = models.DateTimeField(verbose_name='дата и время появления')
     disappear_at = models.DateTimeField(blank=True,
                                         null=True,
-                                        verbose_name='дата и время исчезновения')
+                                        verbose_name='дата и время исчезновения'
+                                        )
     level = models.IntegerField(default=1, verbose_name="Уровень")
     health = models.IntegerField(default=1, verbose_name="Здоровье")
     strength = models.IntegerField(default=1, verbose_name="Атака")
