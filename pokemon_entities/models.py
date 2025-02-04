e83e5157c1cbf7a620c6b35163e516360d995584
Romigo97 <roma.kolyada939@gmail.com>
2025-02-04 22:37:20 +0300

    ixed models and add exception handling

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,20 +4,19 @@ from django.db import models
 class Pokemon(models.Model):
     title_ru = models.CharField(max_length=200,
                                 verbose_name='имя покемона на русском',
-                                null=True
+                                blank=True
                                 )
-    title_en = models.CharField(null=True,
-                                blank=True,
+    title_en = models.CharField(blank=True,
                                 max_length=200,
                                 verbose_name='имя покемона на английском'
                                 )
-    title_jp = models.CharField(null=True,
-                                blank=True,
+    title_jp = models.CharField(blank=True,
                                 max_length=200,
                                 verbose_name='имя покемона на японском'
                                 )
     images = models.ImageField(blank=True,
                                null=True,
+                               upload_to='pocemons_images/',
                                verbose_name='изображение покемона'
                                )
     description = models.TextField(verbose_name='описание')
@@ -46,8 +45,8 @@ class PokemonEntity(models.Model):
                                         null=True,
                                         verbose_name='дата и время исчезновения'
                                         )
-    level = models.IntegerField(default=1, verbose_name="Уровень")
-    health = models.IntegerField(default=1, verbose_name="Здоровье")
-    strength = models.IntegerField(default=1, verbose_name="Атака")
-    defence = models.IntegerField(default=1, verbose_name="Защита")
-    stamina = models.IntegerField(default=1, verbose_name="Выносливость")
+    level = models.PositiveIntegerField(default=1, verbose_name="Уровень")
+    health = models.PositiveIntegerField(default=1, verbose_name="Здоровье")
+    strength = models.PositiveIntegerField(default=1, verbose_name="Атака")
+    defence = models.PositiveIntegerField(default=1, verbose_name="Защита")
+    stamina = models.PositiveIntegerField(default=1, verbose_name="Выносливость")
--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -1,8 +1,7 @@
 import folium
-import json
 
 from django.http import HttpResponseNotFound
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Pokemon, PokemonEntity
 from django.utils import timezone
 
@@ -65,9 +64,9 @@ def show_all_pokemons(request):
 
 
 def show_pokemon(request, pokemon_id):
-    pokemon = Pokemon.objects.get(id=pokemon_id)
+    pokemon = get_object_or_404(Pokemon, id=pokemon_id)
     pokemon_entites = PokemonEntity.objects.filter(pokemon=pokemon)
-
+    
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
     for pokemon_entity in pokemon_entites:
         add_pokemon(
